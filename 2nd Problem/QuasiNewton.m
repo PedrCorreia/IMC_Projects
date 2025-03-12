6a53e280447f0dc961a04f776774bd91cce710fb
ticorreia <156314509+ticorreia@users.noreply.github.com>
2025-03-12 13:25:19 +0000

    Add files via upload

--- 2nd Problem/QuasiNewton.m
+++ 2nd Problem/QuasiNewton.m
@@ -1,12 +1,16 @@
 function [x,er]=QuasiNewton(f,l,x0,tol,nmax) %l é o valor perto de 0
-erro=tol+1; %para entrar no ciclo
+tolr=tol+1; %para entrar no ciclo
 i=1;
-while erro>tol && i<nmax %condições do ciclo
+while tolr>tol && i<nmax %condições do ciclo
     x1=x0-l*f(x0)/(f(x0+l)-f(x0));
     x(i)=x1; %guardar no vetor x
-    erro=abs(x1-x0); %recalcular o erro
-    er(i)=erro; %guardar o erro
+    tolr=abs(x1-x0); %recalcular a tolerância
     x0=x1; %renomear
     i=i+1; 
+end
+i=i-1;
+er=zeros(1,i); %guardar a estimativa do erro absoluto
+for j=1:i
+    er(j)=abs(x(j)-x(end));
 end
 end
